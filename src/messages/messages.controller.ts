a53c5d3f99d5dd20f7003a263b6caaa01d345cfb
Santosh Pasupunuri <spasupunuri@deloitte.co.uk>
2022-12-09 08:20:12 +0000

    messages initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesServices: MessagesService) {}
+  @Get()
+  listMessages() {
+    return this.messagesServices.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesServices.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesServices.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
+  }
+}
